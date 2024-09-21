c7e2a202cbad172196c863962b771ff7662059bc
AreebKhanAnwar <areebkhanp1@gmail.com>
2024-09-21 23:10:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+// Assignment_1
+// Question # 01
+
+// alert("Hello World");
+
+
+
+// Question # 02
+
+// alert("Error! Please Enter a valid password");
+
+
+
+// Question # 03
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+
+
+// Question # 04
+
+// alert("Welcome to JS land...");
+// alert("Happy Coding!");
+
+
+
+// Question # 05
+//Done
+
+
+//Assignment_2
+// Question # 01
+
+// var username = "username";
+// console.log(username);
+
+
+
+// Question # 02
+
+// var myname = "Areeb Khan";
+// console.log(myname);
+
+
+
+// Question # 03
+
+// var message;
+// message = "Hello World"
+// alert(message);
+
+
+
+// Question # 04
+
+// alert("Jhone Doe");
+// alert("15 Year old");
+// alert("Certified Mobile Application Development")
+
+
+
+// Question # 05
+
+// var pizza = " Pizza \n Pizz \n piz \n pi \n p"
+// alert(pizza);
+
+
+
+// Question # 06
+
+// var email = " My email address is areebbrufene@gmail.com"
+// alert(email);
+
+
+
+// Question # 07
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+
+
+// Question # 07
+
+document.write("<h2>Yah! I can write content  through JavaScript</h2>")
